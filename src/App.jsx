aac2fe4c577c238dbd58a165b73f4304847081cb
Khushbu Shukla <shukla.khushbu008@gmail.com>
2025-02-28 14:32:13 -0500

    deleted header.jsx which extra, creates .env by defining server port, and made changes to App.jsx related to fetching photos and comments from server side using .ent

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Photo from "./pages/Photo";
 import "./App.scss";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+  const [tags, setTags] = useState([]);
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      try {
+        const response = await axios.get(
+          `${API_BASE_URL}/photos`);
+        setPhotos(response.data);
+      } catch (error) {
+        console.error("Error fetching photos:", error);
+      }
+    };
+
+    const fetchTags = async () => {
+      try {
+        const response = await axios.get(
+          `${API_BASE_URL}/tags`);
+        setTags(response.data);
+      } catch (error) {
+        console.error("Error fetching tags:", error);
+      }
+    };
+
+    fetchPhotos();
+    fetchTags();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -16,4 +46,3 @@ function App() {
 }
 
 export default App;
-
