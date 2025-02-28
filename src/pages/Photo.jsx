b2776405aea32366673d42a5470f8ef71b5fcc9e
Khushbu Shukla <shukla.khushbu008@gmail.com>
2025-02-28 15:09:34 -0500

    modified Photo.jsx and changed its URL to backend env

--- src/pages/Photo.jsx
+++ src/pages/Photo.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
-import Tag from "../components/Tag/Tag";
 import Comments from "../components/Comments/Comments";
 import "./Photo.scss";
 import Footer from "../components/Footer/Footer";
@@ -13,33 +12,29 @@ function Photo() {
   const [photo, setPhoto] = useState(null);
   const [comments, setComments] = useState(null);
   const [newComment, setNewComment] = useState('');
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
   useEffect(() => {
     fetchPhoto();
     fetchComments();
-  }, []);
+  }, [API_BASE_URL, id]);
 
   async function fetchPhoto() {
-    const { data } = await axios.get(
-      `https://unit-3-project-c5faaab51857.herokuapp.com/photos/${id}?api_key=1c1459ab-a5fe-4f24-a3d6-a9b6f153981e`
-    );
+    const { data } = await axios.get(`${API_BASE_URL}/photos/${id}`);
     setPhoto(data);
   }
 
   async function fetchComments() {
-    const { data } = await axios.get(
-      `https://unit-3-project-c5faaab51857.herokuapp.com/photos/${id}/comments?api_key=1c1459ab-a5fe-4f24-a3d6-a9b6f153981e`
-    );
+    const { data } = await axios.get(`${API_BASE_URL}/photos/${id}/comments`);
     setComments(data);
   }
 
   const handleCommentSubmit = async (comment) => {
-    const {data} = await axios.post(`https://unit-3-project-c5faaab51857.herokuapp.com/photos/${id}/comments`, 
+    const {data} = await axios.post(`${API_BASE_URL}/photos/${id}/comments`, 
     { text: newComment },
     {
       headers: {
         "Content-Type": "application/json",
-        "api_key": "1c1459ab-a5fe-4f24-a3d6-a9b6f153981e"
       }
     }
   );
@@ -58,7 +53,7 @@ function Photo() {
         <PhotoDetail photo={photo} />
       </div>
       <div className="comments-section">
-        <Comments id={id} />
+        <Comments id={id} comments={comments} handleCommentSubmit={handleCommentSubmit} newComment={newComment} setNewComment={setNewComment}/>
       </div>
       <Footer />
     </section>
